8a8558e0fc06398eeb88803dba057bb285eeb2af
Andreaandkjaer <avva48635@edu.ucl.dk>
2023-03-30 12:59:52 +0200

    l

--- uclapp/index.js
+++ uclapp/index.js
@@ -3,7 +3,7 @@
 /*ANDREA*/
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
-const hamburger = document.querySelector(".burger");
+const burger = document.querySelector(".burger");
 const closeIcon = document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
 
@@ -19,7 +19,7 @@ function toggleMenu() {
   }
 }
 
-hamburger.addEventListener("click", toggleMenu);
+burger.addEventListener("click", toggleMenu);
 
 menuItems.forEach(function (menuItem) {
   menuItem.addEventListener("click", toggleMenu);
