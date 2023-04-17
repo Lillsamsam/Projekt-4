0b1095474161caf7f9f4f350c66313939eb16872
Andreaandkjaer <avva48635@edu.ucl.dk>
2023-04-17 21:04:04 +0200

    Update index.js

--- uclapp/index.js
+++ uclapp/index.js
@@ -44,7 +44,7 @@ function Menu() {
 burger.addEventListener("click", Menu);
 
 let select_element = document.getElementById("selectboks");
-let options = ["08:00-09:00", "09:00-10:00", "10:00-11:00", "11:00-12:00", "12:00-13:00", "13:00-14:00"];
+let options = ["08:00-09:00", "09:00-10:00", "10:00-11:00", "12:00-13:00", "13:00-14:00", "14:00-15:00"];
 
 select_element.innerHTML = "";
 
