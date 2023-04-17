d74991770a0cc387a404a71ab0b3a8029f754c13
Andreaandkjaer <avva48635@edu.ucl.dk>
2023-04-17 14:15:24 +0200

    lol1

--- uclapp/index.js
+++ uclapp/index.js
@@ -3,27 +3,31 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
-        for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
 /*ANDREA*/
@@ -38,6 +42,16 @@ function Menu() {
   }
 }
 burger.addEventListener("click", Menu);
+
+let select_element = document.getElementById("selectboks");
+let options = ["08:00-09:00", "09:00-10:00", "10:00-11:00", "11:00-12:00", "12:00-13:00", "13:00-14:00"];
+
+select_element.innerHTML = "";
+
+for (let i = 0; i < options.length; i++) {
+  let opt = options[i];
+  select_element.innerHTML += "<option value=\"" + opt + "\">" + opt + "</option>";
+}
 /*KATHRINE*/
 
 /*MICHELLE*/
