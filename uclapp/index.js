b562c2b3022f7c733e1edf07275cab67947c6345
Danielsalvadorhansen <danielsalvadorhansen@hotmail.com>
2023-04-17 14:03:36 +0200

    r

--- uclapp/index.js
+++ uclapp/index.js
@@ -43,3 +43,51 @@ burger.addEventListener("click", Menu);
 /*MICHELLE*/
 
 /*DANIEL*/
+
+
+// viser koden hvis man klikker checkboxen af
+
+function myFunction() {
+  let x = document.getElementById("psw");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+
+
+// premade koder
+
+
+var userArray = [
+    {
+        username: "user1",
+        password: "pw1"
+    },
+    {
+        username: "user2",
+        password: "pw2"
+    },
+    {
+        username: "user3",
+        password: "pw3"
+    }
+]
+
+
+function loginFunction () {
+     let username = document.getElementById("myText").value;
+     let password = document.getElementById("myText1").value;
+  let currentUser = userArray.filter( user=> user.username == username && user.password == password)
+  currentUser.length ? window.location.href="index.html": console.log('Wrong data');
+}
+
+
+
+
+const button = document.getElementById("button");
+button.addEventListener('click', () => {
+loginFunction();
+});
