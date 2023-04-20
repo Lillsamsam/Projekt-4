015a17f9335b7db9a1bc544a3371ac6dde2b92c4
KathrineJensen <72488973+Lillsamsam@users.noreply.github.com>
2023-04-20 08:33:04 +0200

    Update dropdown.js

--- uclapp/javascript/dropdown.js
+++ uclapp/javascript/dropdown.js
@@ -12,11 +12,9 @@ for(let i = 0; i <optionsA.length; i++){
 
 /*Kathrine*/
 function show(value) {
-    document.querySelector(".dropbox").value = value;
+    document.querySelector(".box").value = value;
   }
 
   let dropdown = document.querySelector(".dropdown")
   dropdown.onclick = function() {
       dropdown.classList.toggle("active")
-  }
-
