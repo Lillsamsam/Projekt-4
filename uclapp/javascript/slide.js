fa8bb5a89c5360bc7c74a2d5c9456304f40a3eaa
KathrineJensen <72488973+Lillsamsam@users.noreply.github.com>
2023-04-20 10:18:46 +0200

    Update slide.js

--- uclapp/javascript/slide.js
+++ uclapp/javascript/slide.js
@@ -31,38 +31,12 @@ function showSlides(n) {
 }
 
 /*KATHRINE*/
-let currentSlide = 0;
-
-function startSlider() {
-  let imageCount = document.querySelectorAll("img");
-
-
-  if (imageCount.length === 0) {
-    imageCount = document.querySelectorAll("img");
-    images.style.transform = `translateX(0px)`;
-    return;
-  }
-
-  let images = document.querySelector("ul");
-  images.style.transform = `translateX(-${currentSlide * 450}px)`;
-
-  if (currentSlide === imageCount.length - 1) {
-    currentSlide = 0;
-  } else {
-    currentSlide++;
-  }
-}
-
-setInterval(() => {
-  startSlider();
-}, 5000);
-/*
 let slideIndex = 0;
 showSlides();
 
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
+  let slides = document.getElementsByClassName("mentorpic");
   let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
@@ -74,6 +48,6 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000);
 }
-*/
+
